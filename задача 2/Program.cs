517e0689396e11dfded48287db90be877c79948f
Artem <artem-vishnevskiy-00@mail.ru>
2022-09-09 15:51:39 +0300

    практическая 3

---
+++ задача 2/Program.cs
@@ -0,0 +1,7 @@
+﻿int a=3;
+int b=2;
+int c= 4;
+int a1 =6;
+int b1= 5;
+int c1 = 8;
+Console.WriteLine(( Math.Sqrt(a1-a)*(a1-a)+(b1-b)*(b1-b)+(c1*c)-(c1*c)));
---
+++ задача3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int a= Convert.ToInt32(Console.ReadLine());
+int c= 1;
+while( c<= a)
+{
+    Console.WriteLine(c*c*c);
+    c++;
+}
